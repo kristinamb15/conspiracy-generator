212d43458641d2cea7a2c5b51e62b6fad58689d0
kristinamb15 <kleifest@gmail.com>
2021-02-14 16:42:56 -0700

    Typo in app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ model, vocab_stoi, vocab_itos = load_model()
 @app.route('/', methods=['GET', 'POST'])
 def generate():
 
-    thumb = request.base_url + 'preview/static/img/thumbnail.png'
+    thumb = request.base_url + 'static/img/thumbnail.png'
 
     if request.method == 'POST':
         seed_text = request.form['seed_text']
